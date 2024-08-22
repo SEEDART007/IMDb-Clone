fae57df28fa4460ff981f8b4347096304035af85
seedart <siddharthachakraborty@gmail.com>
2024-08-23 01:50:11 +0530

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center min-h-screen items-center'>
+      <img src="spinner.svg" alt="Loading..." />
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,10 @@ import Results from './components/Results';
 async function page({searchParams}) {
 const genre=searchParams.genre || 'fetchTrending'
   const API_KEY='c02a1d1fbe3e3dbe2c1f1fe78268bc3d';
-  const res = await fetch(`https://api.themoviedb.org/3${genre==='fetchTopRated'?'/movie/top_rated':'/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`)
+ 
+    const res = await fetch(`https://api.themoviedb.org/3${genre==='fetchTopRated'?'/movie/top_rated':'/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`)
+  
+ 
   const data = await res.json()
   if(!res.ok){
     throw new Error("failed to fetch")
