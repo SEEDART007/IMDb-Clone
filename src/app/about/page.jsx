640f1dc74c457ebce47b0098ef023b844227b22b
seedart <siddharthachakraborty@gmail.com>
2024-08-23 00:45:36 +0530

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-3xl text-amber-600 font-medium'>About</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugiat dolor iusto ducimus sint consequuntur reiciendis libero dolores nam pariatur voluptas, ratione alias culpa cum animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis a nostrum at recusandae iusto quasi laudantium sunt, odit porro non officia vitae, impedit necessitatibus ut illum nobis omnis officiis sint fugit similique ipsum minima error amet debitis. Quod iure culpa aut pariatur neque officiis odit exercitationem iste aspernatur fugit facilis perspiciatis dicta laborum quas et adipisci ut quia, numquam atque dolorem harum consequatur mollitia a. Earum nostrum laborum reprehenderit distinctio quisquam necessitatibus beatae esse repellat fugiat deserunt et, neque cumque tenetur, maxime excepturi, quod optio voluptas exercitationem magnam culpa atque!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugiat dolor iusto ducimus sint consequuntur reiciendis libero dolores nam pariatur voluptas, ratione alias culpa cum animi?</p>
+    </div>
+  )
+}
