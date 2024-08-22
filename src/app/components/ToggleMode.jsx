51abcbcc08faa0832f55b4269ee990044a16d12b
seedart <siddharthachakraborty@gmail.com>
2024-08-23 00:38:03 +0530

    added dark mode

---
+++ src/app/components/ToggleMode.jsx
@@ -0,0 +1,19 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import {MdLightMode,MdDarkMode} from 'react-icons/md'
+import { useTheme } from 'next-themes'
+export default function ToggleMode() {
+    const {theme,setTheme,systemTheme}=useTheme()
+    const[mounted,setMounted]=useState(false)
+    useEffect(()=>{
+      setMounted(true)
+    },[])
+    const currentTheme=theme==='system'?systemTheme:theme
+    
+  return (
+    <div>
+      {mounted &&
+    currentTheme=='dark'?<MdDarkMode onClick={()=>setTheme('light')} className='text-2xl hover:text-amber-500 cursor-pointer'/>:<MdLightMode  onClick={()=>setTheme('dark')} className='text-2xl hover:text-amber-500 cursor-pointer'/>}
+    </div>
+  )
+}
