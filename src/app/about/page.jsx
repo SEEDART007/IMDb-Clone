cf8abac1a0e6acd091ea378f6c1d966d8876dfa0
seedart <siddharthachakraborty@gmail.com>
2024-08-24 00:34:23 +0530

    final2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,10 +4,10 @@ export default function page() {
   return (
     <div className='max-w-6xl mx-auto p-3 space-y-4'>
       <h1 className='text-3xl text-amber-600 font-medium'>About</h1>
-      <p>Discover, track, and explore movies, TV shows, and celebrities with ease. Whether you're a film buff, a binge-watcher, or just looking for your next great watch, myIMDb has everything you need.</p>
+      <p>Discover, track, and explore movies, TV shows, and celebrities with ease. Whether youre a film buff, a binge-watcher, or just looking for your next great watch, myIMDb has everything you need.</p>
       <p>
 Discover myIMDb , your ultimate entertainment hub! Explore a vast library of movies, TV shows, and celebrity profiles. Enjoy personalized recommendations, manage your watchlist, and engage with a vibrant community through reviews and ratings. With advanced search features and up-to-date trending content,myIMDb makes finding and tracking your favorite entertainment easier than ever. Join us today and elevate your viewing experience!</p>
-      <p>myIMDb is your go-to destination for all things entertainment. Seamlessly explore an extensive database of movies, TV shows, and celebrity profiles. With personalized recommendations, a customizable watchlist, and detailed reviews, our app ensures you never miss out on great content. Stay updated with trending shows and movies, use advanced search tools to find exactly what you're looking for, and connect with a community of fellow enthusiasts. myIMDb makes discovering and managing your entertainment easier and more enjoyable than ever.</p>
+      <p>myIMDb is your go-to destination for all things entertainment. Seamlessly explore an extensive database of movies, TV shows, and celebrity profiles. With personalized recommendations, a customizable watchlist, and detailed reviews, our app ensures you never miss out on great content. Stay updated with trending shows and movies, use advanced search tools to find exactly what youre looking for, and connect with a community of fellow enthusiasts. myIMDb makes discovering and managing your entertainment easier and more enjoyable than ever.</p>
     </div>
   )
 }
